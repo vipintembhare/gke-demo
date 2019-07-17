046712e69d7c85fc7ae7b7975bb47d98f0939bb2
vipintembhare <vipinrtembhare@gmail.com>
2019-07-17 16:18:08 +0530

    Update gkecluster.tf

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,5 +1,5 @@
 resource "google_container_cluster" "gke-cluster" {
-  name               = "my-first-gke-cluster"
+  name               = "dev-cluster"
   network            = "default"
   zone               = "europe-west1-b"
   initial_node_count = 3
