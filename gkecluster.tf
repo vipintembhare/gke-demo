8eb0bc1a88c661edbac3b12f7d83592afb25252e
vipintembhare <vipinrtembhare@gmail.com>
2019-07-17 16:55:51 +0530

    machine_Type

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,6 +1,7 @@
 resource "google_container_cluster" "gke-cluster" {
   name               = "dev-cluster"
   network            = "default"
-  zone               = "europe-west1-c"
+  zone               = "europe-west1-b"
+  machine_type       ="f1-micro"
   initial_node_count = 3
 }
